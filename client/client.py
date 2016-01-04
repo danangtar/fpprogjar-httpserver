438ce36729afed05a0fe8afe46314175b0b2cc1f
faizalbhaswara <faisalanugrahb@yahoo.com>
2016-01-04 22:10:35 +0700

    Update client.py
    client menggunakan OO

--- client/client.py
+++ client/client.py
@@ -2,42 +2,75 @@
 import socket
 import sys
 
-response = ''
-request = ''
-
-try:
-    while True:
+class MyHTTPClient():
+    def __init__(self):
         # creating socket client
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket = []
+        self.response = ''
+        self.request = ''
+        self.host = ''
+        self.host = 80
 
-        # connect to server in defined address and port
-        client_socket.connect(('localhost', 5000))
-
-        # send message to server
-        #client_socket.send("Hi server ... ")
-        inputan = raw_input()
+    def input_request(self, inputan):
+        # request
         inputan = inputan.split()
-        link = " ".join(inputan[1:])
+        link = "%20".join(inputan[1:])
+        link = link.split('/')
+        host = link[0]
+        if(':' in host):
+            host = host.split(':')
+            port = int(host[1])
+            host = host[0]
+        else:
+            port = 80
+        link = "/".join(link[1:])
+        link = "/" + link
+
         method = inputan[0]
 
         request = method
-        request += ' / HTTP/1.1\r\nHost: '
-        request += link + '\r\n\r\n'
-        print request
-        client_socket.send(request)
+        request += ' ' + link + ' HTTP/1.1\r\nHost: '
+        request += host + ':' + str(port) + '\r\n\r\n'
+        self.request = request
+        self.host = host
+        self.port = port
 
-        response = client_socket.recv(1024)
+    def connect(self):
+        # connect to server in defined address and port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
+
+    def send_request(self):
+        # send message to server
+        self.client_socket.send(self.request)
+
+        response = self.client_socket.recv(1024)
         while (response):
-            temp = client_socket.recv(1024)
+            temp = self.client_socket.recv(1024)
             if (temp == ''):
                 break
             response += temp
 
-        print response
+        self.response = response
 
-        client_socket.close()
+    def print_response(self):
+        return self.response
 
+    def exit(self):
+        self.client_socket.close()
+
+client = MyHTTPClient()
+
+try:
+    while True:
+        inputan = raw_input()
+
+        client.input_request(inputan)
+        client.connect()
+        client.send_request()
+        print client.print_response()
+        client.exit()
 
 except KeyboardInterrupt:
-    client_socket.close()
+    client.exit()
     sys.exit(0)
